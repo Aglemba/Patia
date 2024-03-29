aacb140a1f954735814a6f45844255d3c12df49f
Aglemba <adrien.glemba@gmail.com>
2024-03-29 09:48:22 +0100

    Patch du make.sh dans Exercices

--- Exercices/make.sh
+++ Exercices/make.sh
@@ -4,7 +4,7 @@
 # Hanoi
 # NPuzzle : TODO Presque FINI
 # GraphColoring
-# PursuitEvasion : TODO
+# PursuitEvasion
 # MachineDeTuring
 # SatSolver
 
@@ -13,7 +13,14 @@ java -cp pddl4j/pddl4j-4.0.0.jar fr.uga.pddl4j.planners.statespace.HSP \
    $1/domain.pddl \
    $1/NotreProbleme$2.pddl
 
-echo -e "Problème $1 : NotreProbleme$2\n"
+# Si tout s'est bien passé, affichez le message suivant : echo -e "Problème $1 : NotreProbleme$2\n"
+# Sinon, affichez le message suivant : Error
+if [ $? -eq 0 ]; then
+    echo -e "*** $1 : Succès ***\n"
+else
+    echo "*** $1 : Echec ***\n"
+    echo "Erreur lors de l'exécution du problème $1 : $1/NotreProbleme$2.pddl\n"
+fi
 
 # Pour exécuter le fichier NotreProbleme de Blocksworld, tapez la commande suivante :
 # ./make.sh Blocksworld
@@ -21,4 +28,3 @@ echo -e "Problème $1 : NotreProbleme$2\n"
 # Pour exécuter le fichier NotreProbleme[NuméroDuProblème] Blocksworld, tapez la commande suivante :
 # ./make.sh Blocksworld [NuméroDuProblème]
 # ./make.sh Blocksworld 1
-
